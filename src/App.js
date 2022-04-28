6ec24c1c6f5881b8461fec3a8e548e56dfb311e8
coltenvantussenbrook <coltenvantussenbrook@gmail.com>
2022-04-28 16:10:17 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Checkbox } from '@moderntribe/wme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>WME FRAMEWORK PLAYGROUND</h1>
+      <div className="components-list">
+        <ul>
+          <li><a href="#button">Button</a></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="components">
+        <div id="button">
+          <h3>Button</h3>
+          <Button
+            variant="contained"
+            color="primary"
+          >
+            Button Text
+          </Button>
+        </div>
+        <div id="checkbox">
+          <h3>Checkbox</h3>
+          <Checkbox
+            label="this is a checkbox"
+          >
+            Button Text
+          </Checkbox>
+        </div>
+      </div>
     </div>
   );
 }
