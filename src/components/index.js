bf441fa9db6c84795731a63c0b00cbfa38d9d792
Alex Delgado <alex@mangomedia.group>
2022-06-01 11:01:33 -0500

    Initialize CRA typescript app

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MyButton } from './Button';
-export { default as MySplitButton } from './SplitButton';
-export { default as MyDropdown } from './Dropdown';
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { WMEProvider } from '@moderntribe/wme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <WMEProvider><App /></WMEProvider>
+    <App />
   </React.StrictMode>
 );
 
